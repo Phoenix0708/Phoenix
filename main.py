7b617ab8300144ab4121af7ca508de3de9a19908
Phoenix <phoenixgamerxz20@gmail.com>
2022-01-18 20:18:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from discord.ext import tasks
 from itertools import cycle
 import os
 
-client=commands.Bot(command_prefix='.')
 
+owners=['814353938429771776']
+client=commands.Bot(command_prefix='.',set_owners=owners)
 status=cycle(['.help','Phoenix'])
 
 @client.event
@@ -15,7 +16,7 @@ async def on_ready():
 
 @tasks.loop(seconds=7)
 async def change_status():
-  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening,name=status))
+  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening,name='Phoenix Chacha'))
 
 
 client.remove_command('help')
@@ -23,5 +24,6 @@ client.remove_command('help')
 for filename in os.listdir('./cogs'):
   if filename.endswith('.py'):
     client.load_extension(f'cogs.{filename[:-3]}')
+
 
 client.run('OTIyODQzNTQ3MjQ4NTE3MTcw.YcHXBw.QeXfaLJ22FuWi9WofhJ6B9dGiNE')    
